707972771566ea436545edeca1d6dfc8eca4fa38
s364582 <s364582@oslomet.no>
2022-02-17 14:56:44 +0100

    Initial commit

---
+++ src/main/java/oslomet/webprog/HeiController.java
@@ -0,0 +1,14 @@
+package oslomet.webprog;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HeiController {
+    @GetMapping("/")
+    public String hei(String navn) {
+        int antallBokstaver = navn.length();
+
+        return "Hei verden " + navn + "! Navnet ditt er " + antallBokstaver + " bokstaver langt :)";
+    }
+}
